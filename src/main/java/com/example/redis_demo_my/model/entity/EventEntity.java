7abb6979198a6e3c2fc7a70565a25ac3b9001465
Dmytro Antonov <dmytro_antonov@epam.com>
2025-06-22 01:02:49 +0300

    addeding postgres

--- src/main/java/com/example/redis_demo_my/model/entity/EventEntity.java
+++ src/main/java/com/example/redis_demo_my/model/entity/EventEntity.java
@@ -1,26 +1,36 @@
 package com.example.redis_demo_my.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
-import java.util.Objects;
+
+import static jakarta.persistence.FetchType.EAGER;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 
-@RedisHash("Event")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
+@Entity
+@Table(name = "events")
 public class EventEntity implements Serializable {
     @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String name;
     private String description;
 
+    @ManyToOne(fetch = EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity user;
 }
--- src/main/java/com/example/redis_demo_my/model/entity/UserEntity.java
+++ src/main/java/com/example/redis_demo_my/model/entity/UserEntity.java
@@ -1,27 +1,38 @@
 package com.example.redis_demo_my.model.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.EAGER;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
-@RedisHash("User")
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "users")
 public class UserEntity implements Serializable {
     @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String name;
 
     @ToString.Exclude
+    @OneToMany(mappedBy = "user", cascade = ALL, fetch = EAGER)
     private Set<EventEntity> events = new HashSet<>();
-
 }
