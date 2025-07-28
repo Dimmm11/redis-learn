895d555ee170bc577f7ebf45258649d3cfe084ea
D Antonov <okpanda11@gmail.com>
2025-07-28 23:38:47 +0300

    refactoring

--- src/main/java/com/example/redis_demo_my/filter/JWTTokenValidatorFilter.java
+++ src/main/java/com/example/redis_demo_my/filter/JWTTokenValidatorFilter.java
@@ -1,9 +1,8 @@
 package com.example.redis_demo_my.filter;
 
-import static com.example.redis_demo_my.utils.Constants.ACCESS_TOKEN;
+import static com.example.redis_demo_my.utils.Constants.AUTHORIZATION;
 import static com.example.redis_demo_my.utils.Constants.ROLES;
 import static com.example.redis_demo_my.utils.Constants.USERNAME;
-import static com.example.redis_demo_my.utils.RequestUtils.extractFieldValue;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -12,6 +11,7 @@ import java.util.stream.Collectors;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,7 +20,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.example.redis_demo_my.configuration.properties.JwtProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -30,19 +29,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.NonNull;
 
 @Slf4j
 @RequiredArgsConstructor
 public class JWTTokenValidatorFilter extends OncePerRequestFilter implements JwtTokenFilter {
 
     private final JwtProperties jwtProperties;
-    private final ObjectMapper objectMapper;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
-            throws ServletException, IOException {
-        String jwt = extractFieldValue(request, ACCESS_TOKEN, objectMapper);
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader(AUTHORIZATION);
         if (jwt != null) {
             try {
                 SecretKey secretKey = getSecretKey();
